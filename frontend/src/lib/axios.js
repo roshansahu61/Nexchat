4b40b00478bc294e6f77046f14477fd8404dc86a
roshansahu61 <roshankumarsahuu@gmail.com>
2025-07-21 15:16:02 +0530

    Configured axios to use VITE_API_URL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+// Use VITE_API_URL from environment variables in production
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : import.meta.env.VITE_API_URL;
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL,
   withCredentials: true,
 });
